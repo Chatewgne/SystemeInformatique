f6b83a458661d31ac28be0adbc477b301e5926c6
Celia Prat <chatewgne@gmail.com>
2019-04-29 17:16:02 +0000

    changed todo list

--- input_code.c
+++ input_code.c
@@ -1,11 +1,11 @@
 main(){
 //a comment
-int dada = 2 ; 
+int dada = 2 + 2 ; 
 int dodo = 1 ;
 if ( 0 ) {
 dodo = 1;
 
-//TODO gérer les "==" et les "<=" (pour le moment les valeurs de droite comme de gauche sont loadées dans R0 donc ça ne marche pas. 
+//TODO gérer les "==". 
 if (dada <= 2) {
 dodo = 5 ;
 } else {
