95d7b13e7cd60a65f7afdf96ee724dbaa9aa5003
midiphony <midiphony@gmail.com>
2019-04-30 14:51:25 +0200

    Improve display of the execution and of the final state of the memory

--- virtualtako.py
+++ virtualtako.py
@@ -39,9 +39,18 @@ def parse_asm_file(file):
 if __name__ == '__main__':
 
     ### Open and parse the assembly file in instruction_table
-    if len(sys.argv) != 2:
-        print('usage: virtualtako <assemblyfile>')
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print('usage: virtualtako <assemblyfile> (optional)--debug')
         exit(1)
+
+    if len(sys.argv) == 3 :
+        if sys.argv[2] != "--debug" :
+            print('usage: virtualtako <assemblyfile> (optional)--debug')
+            exit(1)
+        else :
+            debug = True
+    else :
+        debug = False
 
     file = open_file(sys.argv[1])
     if file is None:
@@ -71,72 +80,138 @@ if __name__ == '__main__':
     while index < inst_tab_length:
         inst = instruction_table[index]
 
-        print(inst.OP, inst.A, inst.B, inst.C)
+        if debug :
+            print("====================")
+            print("@", index, ":", inst.OP, inst.A, inst.B, inst.C)
+        
         
         if inst.OP == 1:     # ADD
-            # print("Add : ", reg[inst.B], " + ", reg[inst.C])
+
+            if debug :
+                print("ADD :\tR", inst.A, " =\tR", inst.B, "(", reg[inst.B], ")\t +\tR", inst.C, "(", reg[inst.C], ")")
+
             reg[inst.A] = reg[inst.B] + reg[inst.C]
             reg[inst.A] &= 0xffff # Only keep the 2 first bytes of the variable in order to stay closer to the real implementation
 
         elif inst.OP == 2:   # MUL
-            # print("Mul : ", reg[inst.B], " * ", reg[inst.C])
+
+            if debug :
+                print("MUL :\tR", inst.A, " =\tR", inst.B, "(", reg[inst.B], ")\t *\tR", inst.C, "(", reg[inst.C], ")")
+
             reg[inst.A] = reg[inst.B] * reg[inst.C]
             reg[inst.A] &= 0xffff
 
         elif inst.OP == 3:   # SOU
-            # print("Sou : ", reg[inst.B], " - ", reg[inst.C])
+
+            if debug :
+                print("SOU :\tR", inst.A, " =\tR", inst.B, "(", reg[inst.B], ")\t -\tR", inst.C, "(", reg[inst.C], ")")
+
             reg[inst.A] = reg[inst.B] - reg[inst.C]
             reg[inst.A] &= 0xffff
 
         elif inst.OP == 4:   # DIV
-            # print("Div : ", reg[inst.B], " // ", reg[inst.C])
+
+            if debug :
+                print("DIV :\tR", inst.A, " =\tR", inst.B, "(", reg[inst.B], ")\t /\tR", inst.C, "(", reg[inst.C], ")")
+
             reg[inst.A] = reg[inst.B] // reg[inst.C]
             reg[inst.A] &= 0xffff
 
 
         elif inst.OP == 5:   # COP
+
+            if debug :
+                print("COP :\tR", inst.A, " <-\tR", inst.B, "(", reg[inst.B], ")")
+
             reg[inst.A] = reg[inst.B]
 
         elif inst.OP == 6:   # AFC
+
+            if debug :
+                print("AFC :\tR", inst.A, " <-\t", (inst.B<<8) + inst.C)
+
             reg[inst.A] = (inst.B<<8) + inst.C
 
         elif inst.OP == 7:   # LOAD
             reg[inst.A] = memory[(inst.B<<8) + inst.C + 1]
             reg[inst.A] += (memory[(inst.B<<8) + inst.C]<<8)
 
+            if debug :
+                print("LOAD :\tR", inst.A, " <-\t@", (inst.B<<8) + inst.C, "\t(", reg[inst.A], ")")
+
         elif inst.OP == 8:   # STORE
             memory[(inst.A<<8) + inst.B + 1] = (reg[inst.C] & 0x00ff)
             memory[(inst.A<<8) + inst.B] = (reg[inst.C] & 0xff00)
 
+            if debug :
+                print("STORE :\t@", (inst.A<<8) + inst.B, " <-\tR", inst.C, "\t(", reg[inst.C], ")")
+
         elif inst.OP == 9:   # EQU
+
             reg[inst.A] = 1 if reg[inst.B] == reg[inst.C] else 0
 
+            if debug :
+                print("EQU :\tR", inst.A, "= 1 if\tR", inst.B, "(", reg[inst.B], ")\t =\tR", inst.C, "(", reg[inst.C], ") else 0")
+
+
         elif inst.OP == 10:  # INF
+
             reg[inst.A] = 1 if reg[inst.B] < reg[inst.C] else 0
 
+            if debug :
+                print("INF :\tR", inst.A, "= 1 if\tR", inst.B, "(", reg[inst.B], ")\t <\tR", inst.C, "(", reg[inst.C], ") else 0")
+
         elif inst.OP == 11:  # INFE
+        
             reg[inst.A] = 1 if reg[inst.B] <= reg[inst.C] else 0
 
+            if debug :
+                print("INFE :\tR", inst.A, "= 1 if\tR", inst.B, "(", reg[inst.B], ")\t <=\tR", inst.C, "(", reg[inst.C], ") else 0")
+
         elif inst.OP == 12:  # SUP
+
             reg[inst.A] = 1 if reg[inst.B] > reg[inst.C] else 0
 
+            if debug :
+                print("SUP :\tR", inst.A, "= 1 if\tR", inst.B, "(", reg[inst.B], ")\t >\tR", inst.C, "(", reg[inst.C], ") else 0")
+
         elif inst.OP == 13:  # SUPE
+
             reg[inst.A] = 1 if reg[inst.B] >= reg[inst.C] else 0
+
+            if debug :
+                print("SUPE :\tR", inst.A, "= 1 if\tR", inst.B, "(", reg[inst.B], ")\t >=\tR", inst.C, "(", reg[inst.C], ") else 0")
 
 
         # JUMP instructions
         if inst.OP == 14:  # JMP
+
             index = (inst.A<<8) + inst.B
 
+            if debug :
+                print("JMP :\t@", inst.A, (inst.A<<8) + inst.B)
+
         elif inst.OP == 15:  # JMPC
+
             index = (inst.A<<8) + inst.B if reg[inst.C] == 0 else (index + 1)
+
+            if debug :
+                print("JMPC :\t@", inst.A, (inst.A<<8) + inst.B, "if R", inst.C, "(", reg[inst.C], ") = 0")
 
         else:
             index += 1
 
     ### End of iteration on instruction_table
 
-    print("End of execution.\n\n")
+    if debug :
+        print("====================")
+
+    print("\nEnd of execution.\n\n")
+
+    print("Memory table :")
+    
+    for i in range(4000,4020):
+        print("|", i, "\t|", memory[i], "\t|")
     
     
     user_input = input("Read value from (r)eg, (m)em or (e)xit : ")
